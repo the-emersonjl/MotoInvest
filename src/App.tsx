02b74a69927fb3c9b42f89549d7ed83be3d5ee96
Emerson Lima <the.emersonjl@gmail.com>
2026-02-26 16:05:35 -0300

    Add main React component to src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// Main React component
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to MotoInvest</h1>
+        </div>
+    );
+};
+
+export default App;
